b1def5819de0f4fdfb098cf78853033c3dc5cd5b
RuaaSaleem <ruaa.saleem21@gmail.com>
2023-12-19 08:44:51 +0200

    java script fiest task

---
+++ main.js
@@ -0,0 +1,10 @@
+ console.log(document.querySelector('div'));
+
+ //add demo class
+ document.querySelector('div').classList.add('demo');
+
+
+
+ console.log(document.querySelector('h1'));
+
+ document.querySelector('h1').classList.remove('vip');
